12f7bbb3ddf5176dd6cde18db9bf7115e915c63e
RB Metcalf <bmetcalf@gmail.com>
2020-08-12 20:05:15 +0200

    Added some stuff.

--- microlens_demo.cpp
+++ microlens_demo.cpp
@@ -13,6 +13,7 @@
 #include "particle_halo.h"
 #include "utilities.h"
 #include "concave_hull.h"
+#include "uniform_lens.h"
 //#include "gadget.hh"
 
 using namespace std;
@@ -23,16 +24,16 @@ int main(int arg,char **argv){
     time(&t);
   
   COSMOLOGY cosmo(Planck18);
-  const long Nstars = 100000;
+  const long Nstars = 5000;//00;
   const double zl = 0.5;  // redshift of lens
   const double zs = 1.0;  // redshift of source
   const int Ninit = 1000;  // initioal size of grid of rays
-  const double kappa_star = 1;  // optical depth
+  const double kappa_star = 0.5;  // optical depth
   const double mass = 1;         /// star mass in solar masses
   
   
   double Re = sqrt( 1.0 / cosmo.SigmaCrit(zl,zs)/ PI );  // Einstein radius of stars
-  double R = Re * sqrt( mass * Nstars / kappa_star);      // radius of region to but stars in
+  double Rphys = Re * sqrt( mass * Nstars / kappa_star);      // radius of region to but stars in
   
   
   //long seed = -11920;
@@ -43,23 +44,36 @@ int main(int arg,char **argv){
   Point_2d center = {0,0};
   Point_2d rotation = {0,0};   // no rotation of stars
   for(StarType &s : stars){
-    double r = R*sqrt(ran());
+    double r = Rphys*sqrt(ran()) ;
     double theta = 2*PI*ran();
     s.x[0] = r*cos(theta) + center[0];
     s.x[1] = r*sin(theta) + center[1];
     s.x[2] = 0;  // in a plane perpendicular to the line of sight
-    s.Mass = mass;
+    s.Mass = mass; 
   }
   
   LensHaloParticles<StarType> lens_halo(stars,zl,cosmo,rotation,false,0,false);
   
+  // put int a compinsating negative density
+  LensHaloKappaDisk disk(zl,-kappa_star*cosmo.SigmaCrit(zl, zs),Rphys,Point_2d(0,0), cosmo);
+  
+  
   Lens lens(&seed,zs,cosmo);
   lens.moveinMainHalo(lens_halo,true);
+  lens.insertMainHalo(disk,false);
   
-  GridMap grid(&lens,Ninit,center.x, Ninit * Re / cosmo.angDist(zl) / 10 );
+  
+  //GridMap grid(&lens,Ninit,center.x, Ninit * Re / cosmo.angDist(zl) / 10 );
+  GridMap grid(&lens,Ninit,center.x, 3*Rphys/cosmo.angDist(zl) );
   
   PixelMap map = grid.writePixelMapUniform(INVMAG);
   map.printFITS("!inverse_mag.fits");
+  grid.writeFitsUniform(KAPPA,"!kappa.fits");
+  grid.writeFitsUniform(GAMMA,"!gamma.fits");
+  grid.writeFitsUniform(GAMMA1,"!gamma1.fits");
+  grid.writeFitsUniform(ALPHA1,"!alpha1.fits");
+  grid.writeFitsUniform(ALPHA,"!alpha.fits");
+
 }
 
 
